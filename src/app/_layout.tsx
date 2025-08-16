5162fe6c12cb93d6d0fc6d4025dc0ee6729146b2
Tiago Rafael <tiago.rafael019@gmail.com>
2025-08-16 00:04:10 -0300

    [STARTED] - Initial file

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,9 @@
-import { Stack } from "expo-router";
+// css
+import "../styles/global.css";
+
+// Expo
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Slot />;
 }
